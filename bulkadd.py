6762bef3178427991124170a3eb61097c28cb137
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2020-12-16 16:45:59 -0500

    blackened

--- bulkadd.py
+++ bulkadd.py
@@ -5,21 +5,28 @@ import csv
 import http.client
 
 # Setup Requests Headers
-headers = {"Content-Type": "application/json",
-                   "Accept-Language": "en-US,en;q=0.5",
-                   "User-Agent": "AndrewPiroli/csv2librenms",
-                   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                   "X-Auth-Token": config.librenms_apikey,
-                   "Connection": "keep-alive"
-                   }
+headers = {
+    "Content-Type": "application/json",
+    "Accept-Language": "en-US,en;q=0.5",
+    "User-Agent": "AndrewPiroli/csv2librenms",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "X-Auth-Token": config.librenms_apikey,
+    "Connection": "keep-alive",
+}
+
 
 def device_add(add_request):
     api_url = f"{config.request_type}://{config.librenms_ipaddress}/api/v0/devices"
-    connection = http.client.HTTPSConnection(api_url) if config.use_https else http.client.HTTPConnection(api_url)
+    connection = (
+        http.client.HTTPSConnection(api_url)
+        if config.use_https
+        else http.client.HTTPConnection(api_url)
+    )
     connection.request("POST", "", add_request, headers)
     response = connection.getresponse()
     data = str(response.data())
     print(f"{response.status} {response.reason} : {data}")
+
 
 def process_csv(csvfile):
     with open(csvfile) as device_list:
@@ -30,24 +37,25 @@ def process_csv(csvfile):
         for entry in reader:
             yield dict(zip(header, entry))
 
+
 if __name__ == "__main__":
     for row in process_csv("data/bulkadd.csv"):
         if row["version"] in ("v1", "v2c"):
             device_info = {
-                "community":row["v1v2community"],
-                "version":row["version"],
+                "community": row["v1v2community"],
+                "version": row["version"],
             }
         elif row["version"] == "v3":
             device_info = {
-                "authlevel":row["v3authlevel"],
-                "authname":row["v3authname"],
-                "authpass":row["v3authpass"],
-                "authalgo":row["v3authalgo"],
-                "cryptopass":row["v3cryptopass"],
-                "cryptoalgo":row["v3cryptoalgo"],
+                "authlevel": row["v3authlevel"],
+                "authname": row["v3authname"],
+                "authpass": row["v3authpass"],
+                "authalgo": row["v3authalgo"],
+                "cryptopass": row["v3cryptopass"],
+                "cryptoalgo": row["v3cryptoalgo"],
             }
         else:
             print("FATAL ERROR: snmp version not recognized")
             continue
-        device_info.update({"hostname":row["hostname"], "version":row["version"]})
+        device_info.update({"hostname": row["hostname"], "version": row["version"]})
         device_add(device_info)
