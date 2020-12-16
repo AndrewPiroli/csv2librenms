e46b4e1eebb9fe1de878152307d277a573795044
AndrewPiroli <45859969+AndrewPiroli@users.noreply.github.com>
2020-12-16 16:45:27 -0500

    rewrite everything

--- bulkadd.py
+++ bulkadd.py
@@ -1,75 +1,53 @@
-#!/usr/bin/python
 import config
 import math
-import requests
-import pandas as pd
+import sys
+import csv
+import http.client
 
 # Setup Requests Headers
-request_headers = {"Content-Type": "application/json",
+headers = {"Content-Type": "application/json",
                    "Accept-Language": "en-US,en;q=0.5",
-                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+                   "User-Agent": "AndrewPiroli/csv2librenms",
                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                    "X-Auth-Token": config.librenms_apikey,
                    "Connection": "keep-alive"
                    }
 
 def device_add(add_request):
-    api_url = "http://{}/api/v0/devices".format(config.librenms_ipaddress)
-    r = requests.post(api_url, json=add_request, headers=request_headers)
-    print(r.text)
+    api_url = f"{config.request_type}://{config.librenms_ipaddress}/api/v0/devices"
+    connection = http.client.HTTPSConnection(api_url) if config.use_https else http.client.HTTPConnection(api_url)
+    connection.request("POST", "", add_request, headers)
+    response = connection.getresponse()
+    data = str(response.data())
+    print(f"{response.status} {response.reason} : {data}")
 
-def device_update(hostname, update_request):
-    api_url = "http://{}/api/v0/devices/{}".format(config.librenms_ipaddress, hostname)
-    r = requests.patch(api_url, json=update_request, headers=request_headers)
-    print(r.text)
+def process_csv(csvfile):
+    with open(csvfile) as device_list:
+        dialect = csv.Sniffer().sniff(device_list.read())
+        device_list.seek(0)
+        reader = csv.reader(device_list, dialect)
+        header = next(reader)
+        for entry in reader:
+            yield dict(zip(header, entry))
 
-# Read CSV file
-try:
-    df = pd.read_csv("data/bulkadd.csv")
-except FileNotFoundError:
-    print("ERROR: data/bulkadd.csv missing")
-    quit()
-
-for index, row in df.iterrows():
-    try:
-        if math.isnan(row['ro']):
-            # Add to Librenms as PING only Device
-            # Create JSON data for API delivery
-            device = {"hostname":row['hostname'],
-                      "sysName":row['sysname'],
-                      "hardware":row['hardware'],
-                      "force_add":"true",
-                      "snmp_disable":"true"
-                      }
-            # Add device to LibreNMS.
-            device_add(device)
-
-            # Update device with OS Type if provided
-            try:
-                if math.isnan(row['os']):
-                    pass
-            except:
-                ostype = {"field": ["os"],
-                          "data": [row['os']]
-                          }
-                device_update(row['hostname'], ostype)
-
-
-            # Update device with syslocation if provided
-            if math.isnan(row['location_id']):
-                pass
-            else:
-                location = {"field": ["location_id", "override_sysLocation"],
-                            "data": [row['location_id'], 1]
-                            }
-                device_update(row['hostname'], location)
-
-    except:
-        # Add Device to LibreNMS via SNMP
-        add_device = {
-            "hostname":row['hostname'],
-            "community":row['ro'],
-            "version":"v2c"
+if __name__ == "__main__":
+    for row in process_csv("data/bulkadd.csv"):
+        if row["version"] in ("v1", "v2c"):
+            device_info = {
+                "community":row["v1v2community"],
+                "version":row["version"],
             }
-        device_add(add_device)
-quit()
+        elif row["version"] == "v3":
+            device_info = {
+                "authlevel":row["v3authlevel"],
+                "authname":row["v3authname"],
+                "authpass":row["v3authpass"],
+                "authalgo":row["v3authalgo"],
+                "cryptopass":row["v3cryptopass"],
+                "cryptoalgo":row["v3cryptoalgo"],
+            }
+        else:
+            print("FATAL ERROR: snmp version not recognized")
+            continue
+        device_info.update({"hostname":row["hostname"], "version":row["version"]})
+        device_add(device_info)
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-# LibreNMS API Settigs
 librenms_apikey = "LIBRENMS API KEY"
 librenms_ipaddress = "LIBRENMS IP ADDRESS"
+use_https = False
+
+#DO NOT CHANGE BELOW THIS LINE
+request_type = "https" if use_https else "http"
